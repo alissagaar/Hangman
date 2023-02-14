cea0cd79e31bfafc4106cfbff437f11e6789104e
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-14 16:52:58 -0500

    Inicios de palabra a adivinar

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
 function HangmanWord() {
+    const word = 'test'
   return (
-    <div>HangmanWord</div>
+    <div style={{display:'flex', gap: '.25em', fontSize:'6rem', fontWeight:'bold', textTransform:'uppercase', fontFamily:'monospace'}}>
+        {word.split('').map((letter, index)=> (<span></span>))}
+    </div>
   )
 }
 
