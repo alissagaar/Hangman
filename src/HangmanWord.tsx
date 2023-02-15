69cc13250d77d223728bd7c1cf28124d7f86fc73
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-15 12:24:02 -0500

    Logic of HangmanWord

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,8 +1,14 @@
 function HangmanWord() {
     const word = 'test'
+    const guessedLetters= ['t']
   return (
     <div style={{display:'flex', gap: '.25em', fontSize:'6rem', fontWeight:'bold', textTransform:'uppercase', fontFamily:'monospace'}}>
-        {word.split('').map((letter, index)=> (<span></span>))}
+        {word.split('').map((letter, index)=> (
+        <span key={index} style={{borderBottom: '.1em solid black'}}>
+          <span style={{visibility:guessedLetters.includes(letter)? 'visible': 'hidden'}}>
+          {letter}
+          </span>
+        </span>))}
     </div>
   )
 }
