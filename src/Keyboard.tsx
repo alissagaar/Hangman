281a6cf3d0ab5c0cd7698e6bf7c8d7120051503f
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-15 12:45:06 -0500

    Keywoard w/o logic

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
-
+import s from './Keyboard.module.css';;
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]
 function Keyboard() {
   return (
-    <div>Keyboard</div>
+    <div style={{
+      display:"grid",
+      gridTemplateColumns: 'repeat(auto-fit, minmax(75px,1fr))',
+      gap: '.5rem'}}>
+        {KEYS.map(key => {
+          return <button className={`${s.btn}`} key={key}>{key}</button>
+        })}
+    </div>
   )
 }
 
