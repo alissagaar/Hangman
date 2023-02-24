30196ee10841845ff56910aded5194afe87d9928
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-24 17:17:38 -0500

    Updating the color of the text

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -7,12 +7,12 @@ type HangmanWordProps = {
 function HangmanWord({guessedLetters, wordToGuess, reveal=false}:HangmanWordProps) {  return (
     <div style={{display:'flex', gap: '.25em', fontSize:'6rem', fontWeight:'bold', textTransform:'uppercase', fontFamily:'monospace'}}>
         {wordToGuess.split('').map((letter, index)=> (
-        <span key={index} style={{borderBottom: '.1em solid black'}}>
+        <span key={index} style={{borderBottom: '.1em solid white'}}>
           <span style={{
             visibility:guessedLetters.includes(letter) || reveal
             ? 'visible'
             : 'hidden',
-            color: !guessedLetters.includes(letter) && reveal ? 'red' : 'black'
+            color: !guessedLetters.includes(letter) && reveal ? 'blue' : 'white'
             }}>
           {letter}
           </span>
