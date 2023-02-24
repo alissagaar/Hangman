3f00d89fe096c2a5983ef48a6df610b3b398b24b
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-24 17:16:45 -0500

    Updating HangmanDrawing

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -3,7 +3,7 @@ const HEAD = (
         width:'50px',
         height:'50px',
         borderRadius:'100%',
-        border: '10px solid black',
+        border: '10px solid white',
         position: 'absolute',
         top:'50px',
         right:'-30px'
@@ -14,7 +14,7 @@ const BODY = (
     <div style={{
         width:'10px',
         height:'100px',
-        background: 'black',
+        background: 'white',
         position: 'absolute',
         top:'120px',
         right:0
@@ -25,7 +25,7 @@ const RIGHT_ARM = (
     <div style={{
         width:'100px',
         height:'10px',
-        background: 'black',
+        background: 'white',
         position: 'absolute',
         top:'150px',
         right:'-100px',
@@ -38,7 +38,7 @@ const LEFT_ARM = (
     <div style={{
         width:'100px',
         height:'10px',
-        background: 'black',
+        background: 'white',
         position: 'absolute',
         top:'150px',
         right:'10px',
@@ -51,7 +51,7 @@ const RIGHT_LEG = (
     <div style={{
         width:'100px',
         height:'10px',
-        background: 'black',
+        background: 'white',
         position: 'absolute',
         top:'210px',
         right:'-90px',
@@ -64,7 +64,7 @@ const LEFT_LEG = (
     <div style={{
         width:'100px',
         height:'10px',
-        background: 'black',
+        background: 'white',
         position: 'absolute',
         top:'210px',
         right:0,
@@ -83,10 +83,10 @@ function HangmanDrawing({numberOfGuesses}: HangmanDrawingProps) {
   return (
     <div style={{position:'relative'}}>
         {BODY_PARTS.slice(0,numberOfGuesses)}
-        <div style={{background:'black', height: '50px', width:'10px', position:'absolute',top: 0, right: 0}} />
-        <div style={{background:'black', height: '10px', width:'200px', marginLeft:'120px'}} />
-        <div style={{background:'black', height: '400px', width:'10px', marginLeft:'120px'}} />
-        <div style={{background:'black', height: '10px', width:'250px'}} />
+        <div style={{background:'white', height: '50px', width:'10px', position:'absolute',top: 0, right: 0}} />
+        <div style={{background:'white', height: '10px', width:'200px', marginLeft:'120px'}} />
+        <div style={{background:'white', height: '400px', width:'10px', marginLeft:'120px'}} />
+        <div style={{background:'white', height: '10px', width:'250px'}} />
     </div>
   )
 }
